9f933dab863eec9b7e101f3bc5adec3d1e22bb8a
Абрамов Игорь <abramov.igor.17@yandex.ru>
2021-10-24 18:59:15 +0700

    Edited assembly info

--- GTA SA Effect Editor/Properties/AssemblyInfo.cs
+++ GTA SA Effect Editor/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HBM")]
-[assembly: AssemblyProduct("Редактор эффектов GTA SA")]
+[assembly: AssemblyProduct("GTA SA Effect editor")]
 [assembly: AssemblyCopyright("Copyright © HBM 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
